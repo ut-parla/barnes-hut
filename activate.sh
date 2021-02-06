047a7b82fc991fcd14d4aee87e3ef10cfbf9bf30
hfingler <hfingler@cs.utexas.edu>
2021-02-05 20:59:52 -0400

    add gen input

--- activate.sh
+++ activate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-python3.7 -m venv .bh && source .bh/bin/activate && pip install -r requirements.txt
+echo "Make sure you're running this script with source: source activate.sh "
+python3.7 -m venv .bh
+source .bh/bin/activate
+pip install -r requirements.txt
