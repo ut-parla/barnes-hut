f790c012bd5fa9cf653f8ed3b6240eb2b166905f
hfingler <hfingler@cs.utexas.edu>
2021-02-23 20:07:36 -0400

    cuda version, saving because I'm installing lib on laptop

--- 2d-barnes-hut-python/barneshut/internals/guvectorize_ops.py
+++ 2d-barnes-hut-python/barneshut/internals/guvectorize_ops.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numba import guvectorize, float64, int64, njit, cuda, jit
-
+from math import sqrt
 
 # TODO: this won't work with CUDA because it requires only one return value and I can't figure
 # out how to do so. We could return it as the last element of cloud_accels, but then the reduction
@@ -27,7 +27,7 @@ def guvect_point_to_cloud(p_pos, p_mass, cloud_positions, cloud_masses, G, p_acc
 
 
 @guvectorize(["float64[:], float64, float64[:,:], float64[:], float64, float64[:,:]"], 
-             '(d),(), (n,d), (n), () -> (n,d)', nopython=True, target="cpu")
+             '(d),(), (n,d), (n), () -> (n,d)', nopython=True, target="cuda")
 def guvect_point_to_cloud_v2(p_pos, p_mass, cloud_positions, cloud_masses, G, cloud_accels):
     # not sure if necessary, there is no documentation on initializing output
     cloud_accels[:, :] = 0.0
@@ -35,8 +35,30 @@ def guvect_point_to_cloud_v2(p_pos, p_mass, cloud_positions, cloud_masses, G, cl
     p_i = n
 
     for i in range(n):
-        dif = p_pos - cloud_positions[i]
-        dist = np.sqrt(np.sum(np.square(dif)))
+        x, y = p_pos[0] - cloud_positions[i][0], p_pos[1] - cloud_positions[i][1]
+        
+        dist = sqrt((x*x) + (y*y))
+
         f = (G * p_mass * cloud_masses[i]) / (dist*dist*dist)
-        cloud_accels[p_i] -= (f * dif / p_mass)
-        cloud_accels[i]   += (f * dif / cloud_masses[i])
+        cloud_accels[p_i][0] -= (f * x / p_mass)
+        cloud_accels[p_i][1] -= (f * y / p_mass)
+        
+        cloud_accels[i][0] += (f * x / cloud_masses[i])
+        cloud_accels[i][1] += (f * y / cloud_masses[i])
+
+
+    # cloud_accels[:, :] = 0.0
+    # n = cloud_positions.shape[0]-1
+    # p_i = n
+
+    # for i in range(n):
+    #     dif = p_pos - cloud_positions[i]
+    #     dist = np.sqrt(np.sum(np.square(dif)))
+    #     f = (G * p_mass * cloud_masses[i]) / (dist*dist*dist)
+    #     cloud_accels[p_i] -= (f * dif / p_mass)
+    #     cloud_accels[i]   += (f * dif / cloud_masses[i])
+
+
+
+
+        
