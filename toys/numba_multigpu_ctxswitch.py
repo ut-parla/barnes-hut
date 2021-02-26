4c0f364cfa8ebddaa1be6a545ce56e8bf67027a0
Henrique Fingler <hfingler@cs.utexas.edu>
2021-02-26 14:20:33 +0000

    start of container

--- toys/numba_multigpu_ctxswitch.py
+++ toys/numba_multigpu_ctxswitch.py
@@ -22,3 +22,8 @@ for dvc in cuda.gpus:
     with cuda.gpus[dvc.id]:
         dvc = cuda.get_current_device()
         print(f"GPU name/id is {dvc.name} / {dvc.id}")
+
+        #ary = np.arange(10)
+        #d_ary = cuda.to_device(ary)
+        #...
+        #hary = d_ary.copy_to_host()
