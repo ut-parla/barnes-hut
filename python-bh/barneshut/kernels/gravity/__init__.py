4e81471aeed2702977a37aa22b3b43c348d71f1d
hfingler <hfingler@cs.utexas.edu>
2021-03-11 20:12:25 -0300

    add numba kernel on switch

--- python-bh/barneshut/kernels/gravity/__init__.py
+++ python-bh/barneshut/kernels/gravity/__init__.py
@@ -13,18 +13,27 @@ def get_gravity_kernel():
         if fc == "vect":
             from . import cpu_vect
             fn = cpu_vect.get_kernel_function()
+
         elif fc == "blas":
             from . import cpu_blas
             fn = cpu_blas.get_kernel_function()
+
+        elif fc == "numba":
+            from . import cpu_numba
+            fn = cpu_numba.get_kernel_function()
+
         elif fc == "guvectorize-cpu":
             from . import guvect_cpu
             fn = guvect_cpu.get_kernel_function("cpu")
+
         elif fc == "guvectorize-parallel":
             from . import guvect_cpu
             fn = guvect_cpu.get_kernel_function("parallel")
+
         elif fc == "guvectorize-cuda":
             from . import guvect_cuda
             fn = guvect_cuda.get_kernel_function()
+            
         elif fc == "nop":
             fn = nop
 
